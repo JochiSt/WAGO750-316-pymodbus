7f15db9965baf2c80e3a65d6d92f59c729b7a440
JochiSt <jochen.steinmann@web.de>
2025-03-25 09:32:34 +0100

    started adding modules

--- test_wago_750-316.py
+++ test_wago_750-316.py
@@ -3,5 +3,20 @@
 """
 
 from WAGO750 import WAGO_750_316
+from WAGO750 import WAGO_750_430, Beckhoff_KL1104
+from WAGO750 import WAGO_750_504, WAGO_750_1515
 
 modbus_io = WAGO_750_316()
+
+# teststack from side of BUSmaster
+
+#WAGO 750-430       8 ch di
+WAGO_750_430(modbus_io)
+#WAGO 750-430       8 ch di
+
+#Beckhoff KL1104    4 ch di
+
+# WAGO 750-504      4 ch do
+WAGO_750_504(modbus_io)
+# WAGO 750-1515     8 ch do
+# WAGO 750-600      endterminal
