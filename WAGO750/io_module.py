e0bb196894df7858bcab90bc07fd92c57862c4ea
JochiSt <jochen.steinmann@web.de>
2025-03-25 09:32:01 +0100

    fixed call of WAGO750 add module

--- WAGO750/io_module.py
+++ WAGO750/io_module.py
@@ -1,10 +1,12 @@
+
+from .WAGO_750 import WAGO_750
+
 class io_module(object):
     """
         Base class for a WAGO / Beckhoff io module
     """
     
-    
-    
-    def __init__(self):
+    def __init__(self, controller : WAGO_750):
+        controller.add_module(self)
         print("Creating a new IO module")
     
