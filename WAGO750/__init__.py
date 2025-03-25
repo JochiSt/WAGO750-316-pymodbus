606b0c083fe998f5f3241bbfc54f452305ce3c95
JochiSt <jochen.steinmann@web.de>
2025-03-25 09:31:43 +0100

    added all classes to __init__

--- WAGO750/__init__.py
+++ WAGO750/__init__.py
@@ -1,3 +1,15 @@
 
+from .io_module import io_module
 from .WAGO_750 import WAGO_750
+
 from .WAGO_750_316 import WAGO_750_316
+
+from .WAGO_750_430 import WAGO_750_430
+from .WAGO_750_504 import WAGO_750_504
+
+from .WAGO_750_600 import WAGO_750_600
+
+from .WAGO_750_1515 import WAGO_750_1515
+
+from .Beckhoff_KL1104 import Beckhoff_KL1104
+
