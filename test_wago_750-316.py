5c5e3c836d4305a7f30db9b98789a895ec64102d
JochiSt <jochen.steinmann@web.de>
2025-03-25 11:22:04 +0100

    save instance of modules

--- test_wago_750-316.py
+++ test_wago_750-316.py
@@ -6,17 +6,21 @@ from WAGO750 import WAGO_750_316
 from WAGO750 import WAGO_750_430, Beckhoff_KL1104
 from WAGO750 import WAGO_750_504, WAGO_750_1515
 
+from WAGO750 import WAGO_750_600
+
 modbus_io = WAGO_750_316()
 
 # teststack from side of BUSmaster
 
 #WAGO 750-430       8 ch di
-WAGO_750_430(modbus_io)
+WAGO_750_430_0 = WAGO_750_430(modbus_io)
 #WAGO 750-430       8 ch di
+WAGO_750_430_1 = WAGO_750_430(modbus_io)
 
 #Beckhoff KL1104    4 ch di
 
 # WAGO 750-504      4 ch do
-WAGO_750_504(modbus_io)
+WAGO_750_504_0 = WAGO_750_504(modbus_io)
 # WAGO 750-1515     8 ch do
 # WAGO 750-600      endterminal
+_ = WAGO_750_600(modbus_io)
