910f4aeefc80e0fd7413a84105e26bb29f90b627
JochiSt <jochen.steinmann@web.de>
2025-04-03 13:28:03 +0200

    updated to latest io_module implementation

--- WAGO750/Beckhoff_KL2134.py
+++ WAGO750/Beckhoff_KL2134.py
@@ -8,8 +8,11 @@ from .WAGO_750 import WAGO_750
 class Beckhoff_KL2134(io_module):
     
     def __init__(self, controller : WAGO_750):
-        super(Beckhoff_KL2134, self).__init__(controller, N_in_regs=None, N_out_regs=4)
+        super(Beckhoff_KL2134, self).__init__(controller, NoutBits=4)
         
     def setBinaryOutputs(self, value = []):
-        print("setting digital outputs")
-        self.controller.client.write_coils( 0x0000 + self.output_offset, value )
+        assert len(value) == 4
+        self.controller.client.write_coils( 0x0000 + self.outBitOffset, value )
+        
+    def getBinaryOutputs(self):
+        return self.controller.client.read_holding_registers( 0x0200 + self.outBitOffset).registers[0]
--- WAGO750/Beckhoff_KL4032.py
+++ WAGO750/Beckhoff_KL4032.py
@@ -11,12 +11,12 @@ from .WAGO_750 import WAGO_750
 class Beckhoff_KL4032(io_module):
     
     def __init__(self, controller : WAGO_750):
-        super(Beckhoff_KL4032, self).__init__(controller, N_in_regs=None, N_out_regs=2)
+        super(Beckhoff_KL4032, self).__init__(controller, NoutWords=2)
         
     def setAnalogOutput(self, channel, value):
         assert channel in [0,1]
-        self.controller.client.write_register( 0x0000 + self.output_offset + channel, value )
+        self.controller.client.write_register( 0x0000 + self.outWordOffset + channel, value )
             
     def getAnalogOutput(self, channel):
         assert channel in [0,1]
-        return self.controller.client.read_input_registers( 0x0200 + self.output_offset + channel).registers[0]
+        return self.controller.client.read_input_registers( 0x0200 + self.outWordOffset + channel).registers[0]
