cf3f3031dbafb07ee3fde84be4d31c67ff9e6d59
JochiSt <jochen.steinmann@web.de>
2025-04-03 13:27:40 +0200

    updated test program

--- test_wago_750-316.py
+++ test_wago_750-316.py
@@ -9,25 +9,34 @@ from WAGO750 import WAGO_750_504, WAGO_750_1515
 from WAGO750 import WAGO_750_600
 
 from WAGO750 import Beckhoff_KL4032
-from WAGO750 import Beckhoff_KL2134
+from WAGO750 import Beckhoff_KL2134, Beckhoff_KL2012
 
 import time
 
+
+################################################################################
 modbus_io = WAGO_750_316(DEBUG=True)
 # teststack from side of BUSmaster
-
-# KL4032_0 = Beckhoff_KL4032(modbus_io)
-# KL4032_0.setAnalogOutput(0,0x7fff)
-# KL4032_0.setAnalogOutput(1,0)
-# print(KL4032_0.getAnalogOutput(0), KL4032_0.getAnalogOutput(1))
 
 KL2134_0 = Beckhoff_KL2134(modbus_io)
 KL2134_1 = Beckhoff_KL2134(modbus_io)
 
+KL4032_0 = Beckhoff_KL4032(modbus_io)
+
+KL2012_0 = Beckhoff_KL2012(modbus_io)
+KL2012_1 = Beckhoff_KL2012(modbus_io)
+
+# WAGO 750-600      endterminal
+_ = WAGO_750_600(modbus_io)
+
+################################################################################
 # reset outputs
 value = [0,0,0,0]
 KL2134_0.setBinaryOutputs( value )
 KL2134_1.setBinaryOutputs( value )
+
+KL2012_0.setBinaryOutputs( value[:2])
+KL2012_1.setBinaryOutputs( value[:-2])
 
 time.sleep(0.5)
 
@@ -36,7 +45,16 @@ for i in range(4):
     value[i] = 1
     KL2134_0.setBinaryOutputs( value )
     KL2134_1.setBinaryOutputs( value )
+    
+    KL2012_0.setBinaryOutputs( value[:2])
+    KL2012_1.setBinaryOutputs( value[-2:])
+    
     time.sleep(0.5)
+    
+KL4032_0.setAnalogOutput(0,0x7fff)
+KL4032_0.setAnalogOutput(1,0)
 
-# WAGO 750-600      endterminal
-_ = WAGO_750_600(modbus_io)
+print(KL4032_0.getAnalogOutput(0), KL4032_0.getAnalogOutput(1))
+
+print(KL2134_0.getBinaryOutputs(), KL2134_1.getBinaryOutputs())
+
