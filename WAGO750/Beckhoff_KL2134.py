e9fb3a466ca5fe06426a293601ef5d8b5ae472de
JochiSt <jochen.steinmann@web.de>
2025-04-03 14:11:44 +0200

    fixed readback of output coils

--- WAGO750/Beckhoff_KL2134.py
+++ WAGO750/Beckhoff_KL2134.py
@@ -8,11 +8,16 @@ from .WAGO_750 import WAGO_750
 class Beckhoff_KL2134(io_module):
     
     def __init__(self, controller : WAGO_750):
-        super(Beckhoff_KL2134, self).__init__(controller, NoutBits=4)
+        self.NoutBits = 4
+        super(Beckhoff_KL2134, self).__init__(controller, NoutBits=self.NoutBits)
         
     def setBinaryOutputs(self, value = []):
         assert len(value) == 4
         self.controller.client.write_coils( 0x0000 + self.outBitOffset, value )
         
     def getBinaryOutputs(self):
-        return self.controller.client.read_holding_registers( 0x0200 + self.outBitOffset).registers[0]
+        outputs = []
+        for i in range(self.NoutBits):
+            outputs.append( self.controller.client.read_coils( 0x0200 + self.outBitOffset + i).bits[0] )
+        return outputs
+        
