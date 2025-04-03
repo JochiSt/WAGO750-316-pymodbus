1eb8376d15b589ec04fbd75a466efb06a7466bc4
JochiSt <jochen.steinmann@web.de>
2025-04-03 14:08:56 +0200

    added two input cards

--- test_wago_750-316.py
+++ test_wago_750-316.py
@@ -8,9 +8,9 @@ from WAGO750 import WAGO_750_504, WAGO_750_1515
 
 from WAGO750 import WAGO_750_600
 
-from WAGO750 import Beckhoff_KL4032
+from WAGO750 import Beckhoff_KL1002
 from WAGO750 import Beckhoff_KL2134, Beckhoff_KL2012
-
+from WAGO750 import Beckhoff_KL4032
 import time
 
 
@@ -26,17 +26,20 @@ KL4032_0 = Beckhoff_KL4032(modbus_io)
 KL2012_0 = Beckhoff_KL2012(modbus_io)
 KL2012_1 = Beckhoff_KL2012(modbus_io)
 
+KL1002_0 = Beckhoff_KL1002(modbus_io)
+KL1002_1 = Beckhoff_KL1002(modbus_io)
+
 # WAGO 750-600      endterminal
 _ = WAGO_750_600(modbus_io)
 
 ################################################################################
 # reset outputs
-value = [0,0,0,0]
+value = [0,0,1,1]
 KL2134_0.setBinaryOutputs( value )
 KL2134_1.setBinaryOutputs( value )
 
 KL2012_0.setBinaryOutputs( value[:2])
-KL2012_1.setBinaryOutputs( value[:-2])
+KL2012_1.setBinaryOutputs( value[-2:])
 
 time.sleep(0.5)
 
@@ -57,4 +60,5 @@ KL4032_0.setAnalogOutput(1,0)
 print(KL4032_0.getAnalogOutput(0), KL4032_0.getAnalogOutput(1))
 
 print(KL2134_0.getBinaryOutputs(), KL2134_1.getBinaryOutputs())
+print(KL1002_0.readBinaryInputs(), KL1002_1.readBinaryInputs())
 
